9ed7925d4a48a4b13cfb710b0c783027b5caa98b
Robin Kahlow <robinkahlow@web.de>
2018-11-08 11:39:39 +0000

    increased shuffle buffer size from 10k to 100k

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def get_input_fn(input_dir, normals_dir, output_dir, shuffle, batch_size, epochs
 
 		dataset = tf.data.Dataset.zip((input_files, normals_files, output_files))
 		if shuffle:
-			dataset = dataset.shuffle(buffer_size=10000)
+			dataset = dataset.shuffle(buffer_size=100000)
 		dataset = dataset.map(parse_image)
 		dataset = dataset.repeat(epochs)
 		dataset = dataset.batch(batch_size)
